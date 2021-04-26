dcd3d15d3551093f061f3c8b52457b11c1e061c8
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-04-26 10:50:08 +0800

    Create GreenApple.java

---
+++ GreenApple.java
@@ -0,0 +1,26 @@
+package Exercise4_1;
+
+public class GreenApple extends Apple { // subclass for Apple
+	private double weight;
+	private String taste;
+    
+	public GreenApple(String name, double p, int q, double w, String t) {
+		super(name, p, q);
+		this.weight = w;
+		this.taste = t;
+		
+	}
+	
+	public double getWeight() {
+		return this.weight;
+	}
+	
+	public String getTaste() {
+		return this.taste;
+	}
+	
+	
+	public String toString() {
+		return super.toString() + "\nWeight: " + weight + "kg" + "\nTaste: " + taste;
+	}
+}
